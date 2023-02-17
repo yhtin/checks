e79ead42b159e1732e87eb64e6c187ad42a06267
Ythin <84581196+Ythin@users.noreply.github.com>
2023-02-17 16:57:33 +0530

    commit

--- app.py
+++ app.py
@@ -2,9 +2,12 @@ from flask import Flask, render_template, request, jsonify
 import json
 import requests
 import os
-import webbrowser
+from selenium import webdriver
+import time
 
 app = Flask(__name__)
+
+
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
@@ -24,6 +27,12 @@ def index():
         env_name = request.form['env_name']
 
         url_list = [url.strip() for url in data[app_name][env_name]["urls"]]
+        # Use Selenium to open a headless Chrome browser and take a screenshot of each URL
+        options = webdriver.ChromeOptions()
+        options.add_argument('headless')
+        options.add_argument('window-size=1200x800')
+        driver = webdriver.Chrome(options=options)
+        screenshot_dict = {}
 
         # Send a GET request to each URL and store the status code
         status_codes = []
@@ -31,16 +40,20 @@ def index():
             try:
                 response = requests.get(url)
                 status_codes.append(response.status_code)
+                driver.get(url)
+                time.sleep(5)  # Wait for the page to load completely
+                screenshot = driver.get_screenshot_as_base64()
+                screenshot_dict[url] = f'data:image/png;base64,{screenshot}'
             except requests.exceptions.RequestException as e:
-                webbrowser.open_new_tab(url)
-                print(f"URL: {url} is not accessible due to an error: {e}")
                 status_codes.append(None)
 
-        return render_template("result.html", url_list=url_list, status_codes=status_codes, zip=zip , app_name=app_name, env_name = env_name)
+        return render_template("result.html", url_list=url_list, status_codes=status_codes, zip=zip,
+                               screenshot_dict=screenshot_dict, app_name=app_name, env_name=env_name)
     else:
         app_list = []
 
     return render_template('index.html', tower_list=tower_list, app_list=app_list)
+
 
 @app.route('/app_names', methods=['GET'])
 def app_names():
@@ -53,6 +66,7 @@ def app_names():
 
     return jsonify(app_names)
 
+
 @app.route('/env_names', methods=['GET'])
 def env_names():
     tower_name = request.args.get('tower_name')
@@ -64,6 +78,7 @@ def env_names():
     env_names = list(data[app_name].keys())
 
     return jsonify(env_names)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
